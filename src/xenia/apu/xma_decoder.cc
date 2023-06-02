ee267a1d12e02767370305bc4e084b9a6e85a2d3
Gliniak <Gliniak93@gmail.com>
2023-06-02 21:33:29 +0200

    Disable decoding on dedicated thread

--- src/xenia/apu/xma_decoder.cc
+++ src/xenia/apu/xma_decoder.cc
@@ -144,7 +144,7 @@ X_STATUS XmaDecoder::Setup(kernel::KernelState* kernel_state) {
       kernel::object_ref<kernel::XHostThread>(new kernel::XHostThread(
           kernel_state, 128 * 1024, 0,
           [this]() {
-            WorkerThreadMain();
+            // WorkerThreadMain();
             return 0;
           },
           kernel_state
@@ -311,6 +311,7 @@ void XmaDecoder::WriteRegister(uint32_t addr, uint32_t value) {
         uint32_t context_id = base_context_id + i;
         auto& context = contexts_[context_id];
         context.Enable();
+        context.Work();
       }
     }
     // Signal the decoder thread to start processing.
