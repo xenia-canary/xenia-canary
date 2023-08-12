f682c947ee4fdf126877e7f7cffe6315afe8d27c
RodoMa92 <marco.rodolfi@tuta.io>
2023-08-12 13:11:01 +0200

    Add AVX CPU checks for posix systems

--- src/xenia/base/main_init_posix.cc
+++ src/xenia/base/main_init_posix.cc
@@ -1,3 +1,9 @@
+#include <gtk/gtk.h>
+#include <gdk/gdk.h>
+#include <xbyak/xbyak/xbyak_util.h>
+
+#include "xenia/ui/window_gtk.h"
+
 /**
  ******************************************************************************
  * Xenia : Xbox 360 Emulator Research Project                                 *
@@ -7,4 +13,35 @@
  ******************************************************************************
  */
 
-// Nothing. Stub.
+class StartupCpuFeatureCheck {
+ public:
+  StartupCpuFeatureCheck() {
+    Xbyak::util::Cpu cpu;
+    const char* error_message = nullptr;
+    if (!cpu.has(Xbyak::util::Cpu::tAVX)) {
+      error_message =
+          "Your CPU does not support AVX, which is required by Xenia. See "
+          "the "
+          "FAQ for system requirements at https://xenia.jp";
+    }
+    if (error_message == nullptr) {
+      return;
+    } else {
+      GtkDialogFlags flags = GTK_DIALOG_DESTROY_WITH_PARENT;
+      auto dialog =
+          gtk_message_dialog_new(nullptr, flags, GTK_MESSAGE_ERROR,
+                                 GTK_BUTTONS_CLOSE,error_message);
+      gtk_dialog_run(GTK_DIALOG(dialog));
+      gtk_widget_destroy(dialog);
+      exit(1);
+    }
+  }
+};
+
+// This is a hack to get an instance of StartupAvxCheck
+// constructed before any initialization code,
+// where the AVX check then happens in the constructor.
+// Ref:
+// https://reviews.llvm.org/D12689#243295
+__attribute__((init_priority(101)))
+static StartupCpuFeatureCheck gStartupAvxCheck;
