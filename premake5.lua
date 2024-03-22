42e7f99021f70fbf2549e6e96cc2189e1e6e14db
Gliniak <Gliniak93@gmail.com>
2024-03-22 11:14:31 +0100

    [3PP] Added Zlib library

--- premake5.lua
+++ premake5.lua
@@ -262,6 +262,7 @@ workspace("xenia")
   include("third_party/xxhash.lua")
   include("third_party/zarchive.lua")
   include("third_party/zstd.lua")
+  include("third_party/zlib.lua")
 
   if not os.istarget("android") then
     -- SDL2 requires sdl2-config, and as of November 2020 isn't high-quality on
