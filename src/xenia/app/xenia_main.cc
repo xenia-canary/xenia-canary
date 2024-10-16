954670a53a5815c1ce8aee32d7977028496c45af
Margen67 <Margen67@users.noreply.github.com>
2024-10-15 22:41:58 -0700

    Make window same size as resolution

--- src/xenia/app/xenia_main.cc
+++ src/xenia/app/xenia_main.cc
@@ -449,13 +449,10 @@ bool EmulatorApp::OnInitialize() {
   emulator_ =
       std::make_unique<Emulator>("", storage_root, content_root, cache_root);
 
-  // Determine window size based on user widescreen setting.
-  uint32_t window_w = 1280;
-  uint32_t window_h = 720;
-  if (!cvars::widescreen) {
-    window_w = 1024;
-    window_h = 768;
-  }
+  // Determine window size based on user setting.
+  auto res = xe::gpu::GraphicsSystem::GetInternalDisplayResolution();
+  uint32_t window_w = res.first;
+  uint32_t window_h = res.second;
 
   // Main emulator display window.
   emulator_window_ = EmulatorWindow::Create(emulator_.get(), app_context(),
