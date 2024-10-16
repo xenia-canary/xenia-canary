7bdd2751c3bb9801b5a461da7015c582bae49231
Margen67 <Margen67@users.noreply.github.com>
2024-10-15 23:25:11 -0700

    [GPU] Custom resolutions

--- src/xenia/gpu/graphics_system.cc
+++ src/xenia/gpu/graphics_system.cc
@@ -30,27 +30,32 @@
 #include "xenia/ui/window.h"
 #include "xenia/ui/windowed_app_context.h"
 
-DEFINE_int32(internal_display_resolution, 8,
-             "Allow game that support different resolutions to be rendered "
-             "in specific resolution.\n"
-             "   0=640x480\n"
-             "   1=640x576\n"
-             "   2=720x480\n"
-             "   3=720x576\n"
-             "   4=800x600\n"
-             "   5=848x480\n"
-             "   6=1024x768\n"
-             "   7=1152x864\n"
-             "   8=1280x720 (Default)\n"
-             "   9=1280x768\n"
-             "   10=1280x960\n"
-             "   11=1280x1024\n"
-             "   12=1360x768\n"
-             "   13=1440x900\n"
-             "   14=1680x1050\n"
-             "   15=1920x540\n"
-             "   16=1920x1080\n",
-             "Video");
+DEFINE_uint32(internal_display_resolution, 8,
+              "Allow games that support different resolutions to render "
+              "in a specific resolution.\n"
+              "   0=640x480\n"
+              "   1=640x576\n"
+              "   2=720x480\n"
+              "   3=720x576\n"
+              "   4=800x600\n"
+              "   5=848x480\n"
+              "   6=1024x768\n"
+              "   7=1152x864\n"
+              "   8=1280x720 (Default)\n"
+              "   9=1280x768\n"
+              "   10=1280x960\n"
+              "   11=1280x1024\n"
+              "   12=1360x768\n"
+              "   13=1440x900\n"
+              "   14=1680x1050\n"
+              "   15=1920x540\n"
+              "   16=1920x1080\n"
+              "   17=internal_display_resolution_x/y",
+              "Video");
+DEFINE_uint32(internal_display_resolution_x, 1280,
+              "Custom width. See internal_display_resolution.", "Video");
+DEFINE_uint32(internal_display_resolution_y, 720,
+              "Custom height. See internal_display_resolution.\n", "Video");
 
 DEFINE_bool(
     store_shaders, true,
@@ -408,7 +413,8 @@ bool GraphicsSystem::Restore(ByteStream* stream) {
 std::pair<uint16_t, uint16_t> GraphicsSystem::GetInternalDisplayResolution() {
   if (cvars::internal_display_resolution >
       internal_display_resolution_entries.size()) {
-    return internal_display_resolution_entries[8];
+    return {cvars::internal_display_resolution_x,
+            cvars::internal_display_resolution_y};
   }
   return internal_display_resolution_entries
       [cvars::internal_display_resolution];
