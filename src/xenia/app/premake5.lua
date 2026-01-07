52f9b5e4936dcca761e969af25fe9fbb1f6c4c0a
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2026-01-06 22:00:36 -0800

    /app/premake5.lua: target Info.plist
    This ensures that premake grabs the Info.plist file correctly with Lua, crucial to getting Xenia Canary to build with VSCode

--- src/xenia/app/premake5.lua
+++ src/xenia/app/premake5.lua
@@ -124,6 +124,14 @@ project("xenia-app")
       })
   end
 
+  filter("system:macosx")
+    files({
+      "Info.plist",
+    })
+    xcodebuildsettings({
+      INFOPLIST_FILE = "../../src/xenia/app/Info.plist",
+    })
+
   filter("platforms:Windows")
     -- Only create the .user file if it doesn't already exist.
     local user_file = project_root.."/build/xenia-app.vcxproj.user"
