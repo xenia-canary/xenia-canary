a61cff33060bc493b2d33d75a855bb45ee8ff0d8
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2026-01-08 01:50:02 -0800

    /app/premake5.lua: gtk and gdk for macOS
    We need to make sure our app folder contains the necessary libraries to link to.

--- src/xenia/app/premake5.lua
+++ src/xenia/app/premake5.lua
@@ -131,6 +131,23 @@ project("xenia-app")
     xcodebuildsettings({
       INFOPLIST_FILE = "../../src/xenia/app/Info.plist",
     })
+    includedirs({
+      "/usr/local/include",
+      "/opt/homebrew/include",
+      "/opt/homebrew/include/gtk-3.0",
+      "/opt/homebrew/include/glib-2.0",
+      "/opt/homebrew/lib/glib-2.0/include",
+      "/opt/homebrew/include/pango-1.0",
+      "/opt/homebrew/include/cairo",
+      "/opt/homebrew/include/gdk-pixbuf-2.0",
+      "/opt/homebrew/include/atk-1.0",
+      "/opt/homebrew/include/harfbuzz",
+    })
+    links({
+      "fontconfig",
+      "gtk",
+      "gdk"
+    })
 
   filter("platforms:Windows")
     -- Only create the .user file if it doesn't already exist.
