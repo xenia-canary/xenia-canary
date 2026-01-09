07a1016ce0bdbf16e9a3347f91ae98a66a25c2df
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2026-01-09 12:52:29 -0800

    /app/premake5.lua: gtk/gdk update
    Our extremely complex approach to dealing with gtk and gdk include issues involves using our own temp directories, and hence, we can remove references to all these locations, since they create an issue with talking to Xcode

--- src/xenia/app/premake5.lua
+++ src/xenia/app/premake5.lua
@@ -134,19 +134,9 @@ project("xenia-app")
     includedirs({
       "/usr/local/include",
       "/opt/homebrew/include",
-      "/opt/homebrew/include/gtk-3.0",
-      "/opt/homebrew/include/glib-2.0",
-      "/opt/homebrew/lib/glib-2.0/include",
-      "/opt/homebrew/include/pango-1.0",
-      "/opt/homebrew/include/cairo",
-      "/opt/homebrew/include/gdk-pixbuf-2.0",
-      "/opt/homebrew/include/atk-1.0",
-      "/opt/homebrew/include/harfbuzz",
     })
     links({
-      "fontconfig",
-      "gtk",
-      "gdk"
+      "fontconfig"
     })
 
   filter("platforms:Windows")
