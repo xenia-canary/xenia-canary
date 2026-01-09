7fbe12f3accb1ee3ba696e9d58b42764b2bb37f7
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2026-01-09 15:59:12 -0800

    /scripts/pkg_config.lua: reference temp gtk folders for macOS

--- tools/build/scripts/pkg_config.lua
+++ tools/build/scripts/pkg_config.lua
@@ -12,6 +12,20 @@ local function pkg_config_call(lib, what)
 end
 
 function pkg_config.cflags(lib)
+  if os.istarget("macosx") and (string.startswith(lib, "gtk") or string.startswith(lib, "gdk") or string.startswith(lib, "glib") or string.startswith(lib, "pango") or string.startswith(lib, "cairo") or string.startswith(lib, "atk")) then
+    os.execute("sh src/xenia/patch/setup_xcode_gtk.sh")
+    includedirs({
+      "/tmp/xenia_gtk_patched_permanent/gtk-3.0",
+      "/tmp/xenia_gtk_patched_permanent/glib-2.0",
+      "/tmp/xenia_gtk_patched_permanent/glib-2.0/include",
+      "/tmp/xenia_gtk_patched_permanent/pango-1.0",
+      "/tmp/xenia_gtk_patched_permanent/cairo",
+      "/tmp/xenia_gtk_patched_permanent/atk-1.0",
+      "/tmp/xenia_gtk_patched_permanent/gdk-pixbuf-2.0",
+      "/tmp/xenia_gtk_patched_permanent/harfbuzz",
+    })
+    return
+  end
   if not os.istarget("linux") then
     return
   end
@@ -21,6 +35,18 @@ function pkg_config.cflags(lib)
 end
 
 function pkg_config.lflags(lib)
+  if os.istarget("macosx") then
+    if string.startswith(lib, "gtk") then
+      links({ "gtk-3", "gdk-3" })
+    elseif string.startswith(lib, "glib") then
+      links({ "glib-2.0", "gobject-2.0", "gio-2.0" })
+    elseif string.startswith(lib, "pango") then
+      links({ "pango-1.0", "pangocairo-1.0" })
+    elseif string.startswith(lib, "cairo") then
+      links({ "cairo" })
+    end
+    return
+  end
   if not os.istarget("linux") then
     return
   end
