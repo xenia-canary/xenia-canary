30a21cbff1066819c84e4da62365634aa04a0890
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-13 23:24:03 -0700

    /base/bit_range.h: macOS header reference

--- src/xenia/base/bit_range.h
+++ src/xenia/base/bit_range.h
@@ -16,7 +16,11 @@
 #include <cstring>
 #include <utility>
 
+#if XE_PLATFORM_MACOS
+#include "xenia/base/math_mac.h"
+#else
 #include "xenia/base/math.h"
+#endif
 
 namespace xe {
 namespace bit_range {
