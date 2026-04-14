15f8ec57b047836316b6bf876774fd83c1d9afd3
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-13 23:21:11 -0700

    /apu/xma_context.h: macOS header reference

--- src/xenia/apu/xma_context.h
+++ src/xenia/apu/xma_context.h
@@ -15,7 +15,11 @@
 #include <mutex>
 #include <queue>
 
+#if XE_PLATFORM_MACOS
+#include "xenia/memory_mac.h"
+#else
 #include "xenia/memory.h"
+#endif
 #include "xenia/xbox.h"
 
 // XMA audio format:
