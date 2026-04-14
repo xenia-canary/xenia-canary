7deb331073bb27090b220fe1f4cf30d9e1aad36f
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-13 17:54:19 -0700

    /apu/audio_driver.h: macOS header reference

--- src/xenia/apu/audio_driver.h
+++ src/xenia/apu/audio_driver.h
@@ -10,7 +10,11 @@
 #ifndef XENIA_APU_AUDIO_DRIVER_H_
 #define XENIA_APU_AUDIO_DRIVER_H_
 
+#if XE_PLATFORM_MACOS
+#include "xenia/memory_mac.h"
+#else
 #include "xenia/memory.h"
+#endif
 #include "xenia/xbox.h"
 
 namespace xe {
