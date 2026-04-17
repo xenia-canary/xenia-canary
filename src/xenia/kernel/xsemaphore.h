793556dbe6319ffc04460d2c6cfd7c23aa146af0
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:21:27 -0700

    /kernel/xsemaphore.h: macOS header references

--- src/xenia/kernel/xsemaphore.h
+++ src/xenia/kernel/xsemaphore.h
@@ -10,9 +10,21 @@
 #ifndef XENIA_KERNEL_XSEMAPHORE_H_
 #define XENIA_KERNEL_XSEMAPHORE_H_
 
+#if XE_PLATFORM_MACOS
+#include "xenia/base/threading_mac.h"
+#else
 #include "xenia/base/threading.h"
+#endif
+#if XE_PLATFORM_MACOS
+#include "xenia/kernel/xobject_mac.h"
+#else
 #include "xenia/kernel/xobject.h"
+#endif
+#if XE_PLATFORM_MACOS
+#include "xenia/kernel/xthread_mac.h"
+#else
 #include "xenia/kernel/xthread.h"
+#endif
 #include "xenia/xbox.h"
 
 namespace xe {
