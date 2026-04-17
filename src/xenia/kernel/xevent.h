8bdfa5e0b43d4ba20c8b2de77f6dbab410c051ee
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:21:16 -0700

    /kernel/xevent.h: macOS header references

--- src/xenia/kernel/xevent.h
+++ src/xenia/kernel/xevent.h
@@ -10,8 +10,16 @@
 #ifndef XENIA_KERNEL_XEVENT_H_
 #define XENIA_KERNEL_XEVENT_H_
 
+#if XE_PLATFORM_MACOS
+#include "xenia/base/threading_mac.h"
+#else
 #include "xenia/base/threading.h"
+#endif
+#if XE_PLATFORM_MACOS
+#include "xenia/kernel/xobject_mac.h"
+#else
 #include "xenia/kernel/xobject.h"
+#endif
 #include "xenia/xbox.h"
 
 namespace xe {
