841ad1b452ee5722f25b9c47b8993baf3b823c10
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:18:20 -0700

    /ui/debug_window.h: macOS header references

--- src/xenia/debug/ui/debug_window.h
+++ src/xenia/debug/ui/debug_window.h
@@ -17,14 +17,30 @@
 #include "xenia/cpu/breakpoint.h"
 #include "xenia/cpu/debug_listener.h"
 #include "xenia/cpu/processor.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/emulator_mac.h"
+#else
 #include "xenia/emulator.h"
+#endif
 #include "xenia/ui/imgui_dialog.h"
 #include "xenia/ui/imgui_drawer.h"
 #include "xenia/ui/immediate_drawer.h"
 #include "xenia/ui/menu_item.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/ui/presenter_mac.h"
+#else
 #include "xenia/ui/presenter.h"
+#endif
+#if XE_PLATFORM_MACOS
+#include "xenia/ui/window_mac.h"
+#else
 #include "xenia/ui/window.h"
+#endif
+#if XE_PLATFORM_MACOS
+#include "xenia/ui/windowed_app_context_mac.h"
+#else
 #include "xenia/ui/windowed_app_context.h"
+#endif
 #include "xenia/xbox.h"
 
 namespace xe {
