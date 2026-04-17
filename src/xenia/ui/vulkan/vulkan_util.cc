30e40686db0e1ab103ae2c977a36fb5a24a25bbd
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:22:03 -0700

    /vulkan/vulkan_util.cc: macOS header reference

--- src/xenia/ui/vulkan/vulkan_util.cc
+++ src/xenia/ui/vulkan/vulkan_util.cc
@@ -12,7 +12,11 @@
 #include <cstdint>
 
 #include "xenia/base/assert.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/base/math_mac.h"
+#else
 #include "xenia/base/math.h"
+#endif
 #include "xenia/ui/vulkan/vulkan_device.h"
 
 namespace xe {
