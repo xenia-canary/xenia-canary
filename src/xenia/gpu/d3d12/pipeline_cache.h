36ebb9fe38eb75f39feec819121eddecb375e26e
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:18:38 -0700

    /d3d12/pipeline_cache.h: macOS header references

--- src/xenia/gpu/d3d12/pipeline_cache.h
+++ src/xenia/gpu/d3d12/pipeline_cache.h
@@ -26,11 +26,19 @@
 #include "xenia/base/hash.h"
 #include "xenia/base/platform.h"
 #include "xenia/base/string_buffer.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/base/threading_mac.h"
+#else
 #include "xenia/base/threading.h"
+#endif
 #include "xenia/gpu/d3d12/d3d12_render_target_cache.h"
 #include "xenia/gpu/d3d12/d3d12_shader.h"
 #include "xenia/gpu/dxbc_shader_translator.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/gpu/gpu_flags_mac.h"
+#else
 #include "xenia/gpu/gpu_flags.h"
+#endif
 #include "xenia/gpu/primitive_processor.h"
 #include "xenia/gpu/register_file.h"
 #include "xenia/gpu/registers.h"
