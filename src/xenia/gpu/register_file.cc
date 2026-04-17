17d344e670d681430fcc9cda7daba80d3c36f2ba
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:18:55 -0700

    /gpu/register_file.cc: macOS header reference

--- src/xenia/gpu/register_file.cc
+++ src/xenia/gpu/register_file.cc
@@ -10,7 +10,11 @@
 #include "xenia/gpu/register_file.h"
 #include <array>
 
+#if XE_PLATFORM_MACOS
+#include "xenia/base/math_mac.h"
+#else
 #include "xenia/base/math.h"
+#endif
 
 namespace xe {
 namespace gpu {
