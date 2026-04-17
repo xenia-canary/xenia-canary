d346b59e61c6241eb270df05ca475024fc8d7bf6
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:19:00 -0700

    /gpu/shader.cc: macOS header references

--- src/xenia/gpu/shader.cc
+++ src/xenia/gpu/shader.cc
@@ -13,8 +13,16 @@
 
 #include "third_party/fmt/include/fmt/format.h"
 #include "xenia/base/filesystem.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/base/math_mac.h"
+#else
 #include "xenia/base/math.h"
+#endif
+#if XE_PLATFORM_MACOS
+#include "xenia/base/memory_mac.h"
+#else
 #include "xenia/base/memory.h"
+#endif
 #include "xenia/base/string.h"
 #include "xenia/gpu/ucode.h"
 
