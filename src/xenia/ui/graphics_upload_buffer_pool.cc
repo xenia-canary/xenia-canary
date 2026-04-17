a4200d3b4465eab266652c355bca452a253b7df0
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:21:51 -0700

    /ui/graphics_upload_buffer_pool.cc: macOS header reference

--- src/xenia/ui/graphics_upload_buffer_pool.cc
+++ src/xenia/ui/graphics_upload_buffer_pool.cc
@@ -10,7 +10,11 @@
 #include "xenia/ui/graphics_upload_buffer_pool.h"
 
 #include "xenia/base/assert.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/base/math_mac.h"
+#else
 #include "xenia/base/math.h"
+#endif
 
 namespace xe {
 namespace ui {
