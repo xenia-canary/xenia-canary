7e33682ad432e55f80fa6ed3ea8ed1c18e077d46
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:18:06 -0700

    /ppc/ppc_opcode_table_gen.cc: macOS header reference

--- src/xenia/cpu/ppc/ppc_opcode_table_gen.cc
+++ src/xenia/cpu/ppc/ppc_opcode_table_gen.cc
@@ -5,7 +5,11 @@
 
 #include "xenia/base/assert.h"
 #include "xenia/cpu/ppc/ppc_opcode.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/cpu/ppc/ppc_opcode_info_mac.h"
+#else
 #include "xenia/cpu/ppc/ppc_opcode_info.h"
+#endif
 
 namespace xe {
 namespace cpu {
