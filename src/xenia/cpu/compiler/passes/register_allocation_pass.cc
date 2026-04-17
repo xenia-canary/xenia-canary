0c057211ebaa6dbf7edf5e7ed7a7e8674fc070ce
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:17:35 -0700

    /passes/register_allocation_pass.cc: macOS header reference

--- src/xenia/cpu/compiler/passes/register_allocation_pass.cc
+++ src/xenia/cpu/compiler/passes/register_allocation_pass.cc
@@ -13,7 +13,11 @@
 
 #include "xenia/base/assert.h"
 #include "xenia/base/logging.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/base/math_mac.h"
+#else
 #include "xenia/base/math.h"
+#endif
 #include "xenia/base/profiling.h"
 
 namespace xe {
