380a213386b121f09d5b6c9267365dd2e838ddc8
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-17 14:31:07 -0700

    /xenia/memory_mac.h
    macOS implementation of memory. This might seem silly, however, given that there may be some macOS exclusive ways we need to handle memory that I haven't gotten to, this file will be a means for you to add anything specific for macOS

---
+++ src/xenia/memory_mac.h
@@ -0,0 +1,15 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2020 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#ifndef XENIA_MEMORY_MAC_H_
+#define XENIA_MEMORY_MAC_H_
+
+#include "xenia/memory.h"
+
+#endif  // XENIA_MEMORY_MAC_H_
