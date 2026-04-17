2f1230bffbaa5135ed24a2eae8ccea5367d53644
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:20:06 -0700

    /apps/xam_app.cc: macOS header reference

--- src/xenia/kernel/xam/apps/xam_app.cc
+++ src/xenia/kernel/xam/apps/xam_app.cc
@@ -10,7 +10,11 @@
 #include "xenia/kernel/xam/apps/xam_app.h"
 
 #include "xenia/base/logging.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/kernel/kernel_state_mac.h"
+#else
 #include "xenia/kernel/kernel_state.h"
+#endif
 #include "xenia/kernel/xam/xam_content_device.h"
 #include "xenia/kernel/xenumerator.h"
 
