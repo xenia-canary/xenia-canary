716a3d20dd3b7f25b1984fdbfa3c6774c980c53f
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:17:40 -0700

    /cpu/entry_table.cc: macOS header reference

--- src/xenia/cpu/entry_table.cc
+++ src/xenia/cpu/entry_table.cc
@@ -10,7 +10,11 @@
 #include "xenia/cpu/entry_table.h"
 
 #include "xenia/base/profiling.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/base/threading_mac.h"
+#else
 #include "xenia/base/threading.h"
+#endif
 
 namespace xe {
 namespace cpu {
