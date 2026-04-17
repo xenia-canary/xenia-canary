9fa329cce59540c1c96bdeaca91c5949e95c7fb8
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-17 15:25:55 -0700

    /x64/x64_sequences_mac.h
    macOS loves to nitpick how we call things so hence the preprocessor macros for the tiny differences

---
+++ src/xenia/cpu/backend/x64/x64_sequences_mac.h
@@ -0,0 +1,65 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2014 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#ifndef XENIA_CPU_BACKEND_X64_X64_SEQUENCES_MAC_H_
+#define XENIA_CPU_BACKEND_X64_X64_SEQUENCES_MAC_H_
+
+#include "xenia/base/logging.h"
+#include "xenia/cpu/hir/instr.h"
+
+#include <unordered_map>
+#define assert_impossible_sequence(name)          \
+  assert_always("impossible sequence hit" #name); \
+  XELOGE("impossible sequence hit: {}", #name)
+
+namespace xe {
+namespace cpu {
+namespace backend {
+namespace x64 {
+
+class X64Emitter;
+
+typedef bool (*SequenceSelectFn)(X64Emitter&, const hir::Instr*, uint32_t ikey);
+
+#ifdef __APPLE__
+// On macOS, use a Meyer's singleton to avoid static initialization order issues
+std::unordered_map<uint32_t, SequenceSelectFn>& GetSequenceTable();
+#else
+extern std::unordered_map<uint32_t, SequenceSelectFn> sequence_table;
+#endif
+
+template <typename T>
+bool Register() {
+#ifdef __APPLE__
+  GetSequenceTable().insert({T::head_key(), T::Select});
+#else
+  sequence_table.insert({T::head_key(), T::Select});
+#endif
+  return true;
+}
+
+template <typename T, typename Tn, typename... Ts>
+static bool Register() {
+  bool b = true;
+  b = b && Register<T>();          // Call the above function
+  b = b && Register<Tn, Ts...>();  // Call ourself again (recursively)
+  return b;
+}
+#define EMITTER_OPCODE_TABLE(name, ...) \
+  const auto X64_INSTR_##name = Register<__VA_ARGS__>();
+
+bool SelectSequence(X64Emitter* e, const hir::Instr* i,
+                    const hir::Instr** new_tail);
+
+}  // namespace x64
+}  // namespace backend
+}  // namespace cpu
+}  // namespace xe
+
+#endif  // XENIA_CPU_BACKEND_X64_X64_SEQUENCES_MAC_H_
