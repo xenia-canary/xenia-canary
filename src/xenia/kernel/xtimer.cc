9fe4da52039ce2732b7b820842d6b31da1cf97ad
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:21:32 -0700

    /kernel/xtimer.cc: macOS header reference

--- src/xenia/kernel/xtimer.cc
+++ src/xenia/kernel/xtimer.cc
@@ -10,7 +10,11 @@
 #include "xenia/kernel/xtimer.h"
 
 #include "xenia/base/logging.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/kernel/xthread_mac.h"
+#else
 #include "xenia/kernel/xthread.h"
+#endif
 
 namespace xe {
 namespace kernel {
