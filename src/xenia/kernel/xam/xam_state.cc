2af3930bb64018f9042e4d0ec09ddd19396cd7e8
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-16 22:20:44 -0700

    /xam/xam_state.cc: macOS header reference

--- src/xenia/kernel/xam/xam_state.cc
+++ src/xenia/kernel/xam/xam_state.cc
@@ -8,7 +8,11 @@
  */
 
 #include "xenia/kernel/xam/xam_state.h"
+#if XE_PLATFORM_MACOS
+#include "xenia/emulator_mac.h"
+#else
 #include "xenia/emulator.h"
+#endif
 
 namespace xe {
 namespace kernel {
