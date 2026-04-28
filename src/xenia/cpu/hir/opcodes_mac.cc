73ee2a45225576762ab007354c8c1911094c02e6
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-04-27 22:50:55 -0700

    /hir/opcodes_mac.cc

---
+++ src/xenia/cpu/hir/opcodes_mac.cc
@@ -0,0 +1,39 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2013 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#include "xenia/cpu/hir/opcodes.h"
+
+namespace xe {
+namespace cpu {
+namespace hir {
+
+#define DEFINE_OPCODE(num, name, sig, flags) \
+  constexpr OpcodeInfo num##_info = {        \
+      num,                                   \
+      flags,                                 \
+      sig,                                   \
+  };
+#include "xenia/cpu/hir/opcodes.inl"
+#undef DEFINE_OPCODE
+
+const char* GetOpcodeName(Opcode num) {
+  switch (num) {
+#define DEFINE_OPCODE(num, name, sig, flags) \
+  case num:                                  \
+    return name;
+#include "xenia/cpu/hir/opcodes.inl"
+#undef DEFINE_OPCODE
+    default:
+      break;
+  }
+  return "invalid opcode";
+}
+}  // namespace hir
+}  // namespace cpu
+}  // namespace xe
