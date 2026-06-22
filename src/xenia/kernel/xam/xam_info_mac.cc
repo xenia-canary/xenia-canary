16bb122abd6e1f78c90aeddb8232ed4e0a54efec
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-22 02:52:18 -0700

    remove macOS Code: Rosette migration

--- src/xenia/kernel/xam/xam_info_mac.cc
+++ src/xenia/kernel/xam/xam_info_mac.cc
@@ -11,7 +11,7 @@
 #include "xenia/base/logging.h"
 #include "xenia/base/string_util.h"
 #include "xenia/kernel/kernel_state_mac.h"
-#include "xenia/kernel/title_id_utils_mac.h"
+#include "xenia/kernel/title_id_utils.h"
 #include "xenia/kernel/user_module_mac.h"
 #include "xenia/kernel/util/shim_utils.h"
 #include "xenia/kernel/xam/xam_module.h"
